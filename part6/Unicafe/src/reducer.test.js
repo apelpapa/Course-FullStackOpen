e9aa4d3ad8f57e0f538c88548dbe71e0ad802ade
apelpapa <apelpapa@gmail.com>
2025-03-30 17:20:27 -0600

    Finished 6.2

--- part6/Unicafe/src/reducer.test.js
+++ part6/Unicafe/src/reducer.test.js
@@ -14,6 +14,7 @@ describe('unicafe reducer', () => {
       type: 'DO_NOTHING'
     }
 
+    deepFreeze(state)
     const newState = counterReducer(undefined, action)
     expect(newState).toEqual(initialState)
   })
@@ -28,6 +29,55 @@ describe('unicafe reducer', () => {
     const newState = counterReducer(state, action)
     expect(newState).toEqual({
       good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('Ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('Bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('Zero resets State', () => {
+    const badAction = {
+      type: 'BAD'
+    }
+    const zeroAction = {
+      type: 'ZERO'
+    }
+    let state = initialState
+
+    deepFreeze(state)
+    state = counterReducer(state, badAction)
+    const newState = counterReducer(state, zeroAction)
+    expect(newState).toEqual({
+      good: 0,
       ok: 0,
       bad: 0
     })
